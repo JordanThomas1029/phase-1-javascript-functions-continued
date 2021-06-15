35c5f31a16cf197417869e1852e9ed8505d7e640
Jordan Thomas <tjordan706@gmail.com>
2021-06-15 00:26:49 -0400

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(symbol = '*') {
+  return function (msg = 'special') {
+    return `You are ${symbol}${msg}${symbol}!`;
+  };
+}
+
+wrapAdjective('*', 'a hard worker')();
+wrapAdjective('||', 'a dedicated programmer');
+
+//('*')('a hard worker');
+//wrapAdjective('||')('a dedicated programmer');
